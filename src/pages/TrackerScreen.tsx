ee71f6b87dc1a0148778e53b4d07436aa8fd8243
steve <stevemcorry@gmail.com>
2021-05-13 10:52:49 -0600

    add passing data to tracker screen

--- src/pages/TrackerScreen.tsx
+++ src/pages/TrackerScreen.tsx
@@ -1,36 +1,73 @@
 import React from 'react';
 import Fire from '../../environment.config';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import AddTagsScreen from './AddTagsScreen';
+import UserDayObj from '../shared/UserDayObj.model';
 
-export default function TrackerScreen() {
-    const uid = "ajsdkfla;jsdflka";
-    const setMood = (mood: number) => {
-        console.log(mood);
-        Fire.database().ref("/users/" + uid + "/Days/mood/").set(mood);
-    }
+class TrackerScreen extends React.Component{
+  
+  constructor(props){
+      super(props);
+      this.state = {
+          tags: null,
+          mood: 5
+      }
+  }
+
+  setMood = (mood: number) => {
+      console.log(mood);
+      this.setState({mood})
+  }
+
+  sendObj = () => {
+
+    console.log(this.state.mood, this.state.tags)
+    let usr = new UserDayObj(this.state.mood, this.state.tags);
+    let uid = "ajsdkfla;jsdflka";
+      Fire.database().ref("/users/" + uid + "/Days/mood/").set(usr);
+  }
+
+  handleCallback = (childData) =>{
+    console.log(childData)
+    this.setState({tags: childData})
+  }
+
+  render(){
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>How did you feel today?</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center', flexDirection: "row" }}>
+          <Button
+            title="Great"
+            onPress={() => this.setMood(5)}
+          />
+          <Button
+            title="Good"
+            onPress={() => this.setMood(4)}
+          />
+          <Button
+            title="Ok"
+            onPress={() => this.setMood(3)}
+          />
+          <Button
+            title="Been better"
+            onPress={() => this.setMood(2)}
+          />
+          <Button
+            title="Not well"
+            onPress={() => this.setMood(1)}
+          />
+        </View>
+        <AddTagsScreen setTags={this.handleCallback}></AddTagsScreen>
+
         <Button
-          title="Great"
-          onPress={() => setMood(5)}
-        />
-        <Button
-          title="Good"
-          onPress={() => setMood(4)}
-        />
-        <Button
-          title="Ok"
-          onPress={() => setMood(3)}
-        />
-        <Button
-          title="Been better"
-          onPress={() => setMood(2)}
-        />
-        <Button
-          title="Not well"
-          onPress={() => setMood(1)}
-        />
+            title="Save Day"
+            onPress={() => this.sendObj()}
+          />
+
       </View>
-    );
+      );
+    }
   }
+
+  export default TrackerScreen
