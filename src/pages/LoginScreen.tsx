b7fd9fe58a1ef15960a0d9446089b43802ebc7b3
Nate Andrus <nate.andrus@modiohealth.com>
2021-05-14 15:10:53 -0600

    Add profile information to profile screen.  Minor fixes.  Syle the login form and registration form.

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { StyleSheet, Button, Text, View, TextInput } from "react-native";
+import { StyleSheet, Button, Text, View, TextInput, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { LoginForm } from "../layouts/LoginForm";
 
 export default function LoginScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <Text>Login to Happiness</Text>
-       <View>
-        <LoginForm navigation={navigation}/>
-       </View>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>      
+        <Text style={styles.mainText}>Login to Happiness</Text>
+        <View>        
+            <LoginForm navigation={navigation}/>        
+        </View>      
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
+const styles = StyleSheet.create({
+  error: {
+      color: 'red'
+  },
+  mainText: {
+    color: 'black',
+    fontSize: 28,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    marginBottom: 10
+  },
+ })
